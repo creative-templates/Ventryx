be8205c33ef2a18f868fd7c14284ed8b6cfbe89c
Heriz Shrestha <19641023+hstha@users.noreply.github.com>
2025-07-20 03:28:46 -0400

    feat: Initialize backend project with Spring Boot and Maven
    - scaffolded project using spring initializer
    - Add Maven Wrapper scripts and properties for easier project setup.
    - Create a .gitignore file to exclude build artifacts and IDE-specific files.
    - Add Checkstyle configuration for code style enforcement.
    - Set up basic Spring Boot application structure with main class and application properties.
    - Include initial test class for application context loading.
    - Define project dependencies and build configuration in pom.xml.
    - Configured .editorconfig

---
+++ backend/src/main/java/com/creative_templates/ventryx/VentryxApplication.java
@@ -0,0 +1,13 @@
+package com.creative_templates.ventryx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentryxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentryxApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/creative_templates/ventryx/VentryxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creative_templates.ventryx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentryxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
